e445ff79374fe76565de231458658187f6469d70
Rishabh <asthana4@illinois.edu>
2018-04-29 10:50:32 -0500

    Image-encoder working

--- SteganoWeb.js
+++ SteganoWeb.js
@@ -16,212 +16,265 @@ var fs = require('fs');
 
 var Jimp = require("jimp");
 
+var image_obj;
+var secret_buffer;
+var allowed_size = 0;
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
 
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload_ballast', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
     
-    console.log(req.files.file);
-    let sampleFile = req.files.sampleFile;
+    let sampleFile = req.files.file;
     //console.log(sampleFile);
     //console.log(sampleFile.name);
- 	// Upload only .txt files to the server filesystem,
-//    if(  sampleFile.name.indexOf('.wav') != -1
-//      || sampleFile.name.indexOf('.png') != -1
-//      || sampleFile.name.indexOf('.jpg') != -1
-//      || sampleFile.name.indexOf('.bmp') != -1){
-////        sampleFile.mv('./public/graphs/' + sampleFile.name, function(err) {
-////   	    if (err)
-////            return res.status(500).send(err);
-////        });
-//        console.log("File uploaded");
-//        res.send('File uploaded!');
-//        return;
-//    }
-    res.send("Unsupported file type");
+ 	// Verify extension
+    if(  sampleFile.name.indexOf('.png') != -1
+      || sampleFile.name.indexOf('.jpg') != -1
+      || sampleFile.name.indexOf('.bmp') != -1){
+        console.log("Valid format");
+        parseImage(sampleFile.data, res);
+        return;
+    }
+    else if(sampleFile.name.indexOf('.wav') != -1){
+        
+    }
+    else{
+        res.send("Unsupported file type");
+    }
 });
+
+app.post('/upload_secret', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('Secret failed to upload');
+    
+    let sampleFile = req.files.file;
+    secret_buffer = sampleFile.data;
+    console.log(secret_buffer);
+    res.send("Secret Uploaded");
+    return;
+    
+});
+
+
+app.get('/encode', function(req, res){
+    encode(secret_buffer, image_obj); 
+});
+
+app.post('/decode', function(req, res){
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+    
+    let sampleFile = req.files.file;
+    //console.log(sampleFile);
+    //console.log(sampleFile.name);
+ 	// Verify extension
+    if(  sampleFile.name.indexOf('.png') != -1){
+        console.log("Decoding image : Valid format");
+        
+        Jimp.read(sampleFile.data, function(err, image){
+            if (err) throw err;
+            decode(image);
+        });
+        
+        return;
+    }
+    else if(sampleFile.name.indexOf('.wav') != -1){
+        
+    }
+    else{
+        res.send("Unsupported file type");
+    }
+});
+
+app.get('/download_encoded', function(req, res){
+  var file = __dirname + '/encoded_secret.png';
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/download_decoded', function(req, res){
+  var file = __dirname + '/output_binary';
+  res.download(file); // Set disposition and send it.
+});
+
 
 app.listen(port, function(){
     console.log("Listening on port " + port);
 });
 
-function encode(secret_buffer){
+function parseImage(image_buffer, res){
+    Jimp.read(image_buffer, function(err, image){
+        if (err) throw err;
+        image_obj = image; 
+        allowed_size = (image.bitmap.width * image.bitmap.height) - 4;
+        res.send(allowed_size.toString());
+    });
+}
 
-    Jimp.read("test.png", function (err, image) {
-	    if (err) throw err;
+function encode(secret_buffer, image){
 
-		let width = image.bitmap.width;
-		let height = image.bitmap.height;
-        
-        // +1 is to account for end of secret symbol '#'
-		if ( width * height < secret_buffer.length + 1){
-			console.log("Image too small");
-		}
-        
-        let current_index = 0;
-        let counter = 0;
-        
-        console.log("Encoded length : ", secret_buffer.length, "Bin", secret_buffer.length.toString(2));
-        for (let i = 0; i < width; i++){
-			for (let j = 0; j < height; j++){
-                
-                let pixel_color = this.getPixelColor(i, j);
-                let stubbed_color = pixel_color & 4244438268;
+    if(!secret_buffer || !image){
+        console.log("Buffer / Image missing, Encoding Failed");
+        return;
+    }
+    let width = image.bitmap.width;
+    let height = image.bitmap.height;
 
-                if (counter < 32){
-                
-                    let to_encode = 0;
-                    
-                    for (let k = 0; k < 4; k++){
-                        
-                        let len_bits = (secret_buffer.length >> counter) & 3;
-                        if (k != 0 )
-                            to_encode = to_encode << 8;
-                        to_encode ^= len_bits;
-                        counter += 2;
-                    }
-                    
-                    //console.log("Original color : ", pixel_color, pixel_color.toString(2));
-                    
+    if ( width * height < secret_buffer.length + 4){
+        console.log("Image too small");
+        return;
+    }
+
+    let current_index = 0;
+    let counter = 0;
+
+    console.log("Encoded length : ", secret_buffer.length, "Bin", secret_buffer.length.toString(2));
+    for (let i = 0; i < width; i++){
+        for (let j = 0; j < height; j++){
+
+            let pixel_color = image.getPixelColor(i, j);
+            let stubbed_color = pixel_color & 4244438268;
+
+            if (counter < 32){
+
+                let to_encode = 0;
+
+                for (let k = 0; k < 4; k++){
+
+                    let len_bits = (secret_buffer.length >> counter) & 3;
+                    if (k != 0 )
+                        to_encode = to_encode << 8;
+                    to_encode ^= len_bits;
+                    counter += 2;
+                }
+
+                //console.log("Original color : ", pixel_color, pixel_color.toString(2));
+
+                let augmented_color = stubbed_color ^ to_encode;
+                image.setPixelColor(augmented_color, i, j);
+
+                //console.log("Augmented color : ", augmented_color, augmented_color.toString(2));
+                //console.log("Modulated color : ", image.getPixelColor(i, j), image.getPixelColor(i, j).toString(2));
+
+                if (counter % 8 == 0){
+                    continue;    
+                }
+            }
+            else{
+                let secret_byte = secret_buffer[current_index];
+                let to_encode = 0;
+
+                if (current_index < secret_buffer.length){
+
+                    let red_bits = secret_byte & 3;
+                    let green_bits = (secret_byte & 12) >> 2;
+                    let blue_bits = (secret_byte & 48) >> 4;
+                    let alpha_bits = (secret_byte & 192) >> 6;
+
+                    to_encode ^= red_bits;
+                    to_encode <<= 8;
+                    to_encode ^= green_bits;
+                    to_encode <<= 8;
+                    to_encode ^= blue_bits;
+                    to_encode <<= 8;
+                    to_encode ^= alpha_bits;
+
                     let augmented_color = stubbed_color ^ to_encode;
-                    this.setPixelColor(augmented_color, i, j);
-                    
-                    //console.log("Augmented color : ", augmented_color, augmented_color.toString(2));
-                    //console.log("Modulated color : ", image.getPixelColor(i, j), image.getPixelColor(i, j).toString(2));
 
-                    if (counter % 8 == 0){
-                        continue;    
+                    if(current_index < 5){
+                        console.log("Secret byte : ", secret_byte.toString(2));
+                        console.log("Augmented color : ", augmented_color.toString(2));   
                     }
-                }
-                else{
-                    let secret_byte = secret_buffer[current_index];
-                    let to_encode = 0;
-                    
-                    if (current_index < secret_buffer.length){
+                    image.setPixelColor(augmented_color, i, j);
 
-                        let red_bits = secret_byte & 3;
-                        let green_bits = (secret_byte & 12) >> 2;
-                        let blue_bits = (secret_byte & 48) >> 4;
-                        let alpha_bits = (secret_byte & 192) >> 6;
+                    current_index++;
+                }    
+            }
 
-                        to_encode ^= red_bits;
-                        to_encode <<= 8;
-                        to_encode ^= green_bits;
-                        to_encode <<= 8;
-                        to_encode ^= blue_bits;
-                        to_encode <<= 8;
-                        to_encode ^= alpha_bits;
-                        
-                        let augmented_color = stubbed_color ^ to_encode;
-                        
-                        if(current_index < 5){
-                            console.log("Secret byte : ", secret_byte.toString(2));
-                            console.log("Augmented color : ", augmented_color.toString(2));   
-                        }
-                        this.setPixelColor(augmented_color, i, j);
-                        
-                        current_index++;
-                    }    
-                }
-                
-                
 
-			}
-		}
-        
-        this.write("del.png")
 
-	});
-
+        }
+    }
+    image.write("encoded_secret.png")
 }
 
 
-function decode(){
-	Jimp.read("del.png", function (err, image) {
-	    if (err) throw err;
+function decode(image){
 
-		let width = image.bitmap.width;
-		let height = image.bitmap.height;
-        
-        let counter = 0;
-        let secret_length = 0;
-        let extracted_byte = 0;
-        let current_index = 0;
-        
-		for (let i = 0; i < width; i++){
-			for (let j = 0; j < height; j++){
-                
+    let width = image.bitmap.width;
+    let height = image.bitmap.height;
 
-                let pixel_color = this.getPixelColor(i, j);
-                //console.log("Pixel color", pixel_color);
-                if (counter < 32){
-                
-                    for (let k = 0; k < 4; k++){   
-                        let len_bits = ( pixel_color >> (k * 8) ) & 3;
-                        //console.log("Len bits", counter/2, len_bits);
-                        extracted_byte = extracted_byte << 2;
-                        extracted_byte ^= len_bits;
-                        counter += 2;
-                    }
-                    if (counter % 8 == 0){
-                        
-                        if (counter == 8){
-                            secret_length = extracted_byte;
-                        }
-                        else{
-                            extracted_byte <<= (counter - 8);
-                            secret_length ^= extracted_byte;
-                        }
-                        extracted_byte = 0;
-                        continue;    
-                    }
+    let counter = 0;
+    let secret_length = 0;
+    let extracted_byte = 0;
+    let current_index = 0;
+
+    for (let i = 0; i < width; i++){
+        for (let j = 0; j < height; j++){
+
+
+            let pixel_color = image.getPixelColor(i, j);
+            //console.log("Pixel color", pixel_color);
+            if (counter < 32){
+
+                for (let k = 0; k < 4; k++){   
+                    let len_bits = ( pixel_color >> (k * 8) ) & 3;
+                    //console.log("Len bits", counter/2, len_bits);
+                    extracted_byte = extracted_byte << 2;
+                    extracted_byte ^= len_bits;
+                    counter += 2;
                 }
-                else{
-                    let secret_byte = 0;
-                    
-                    if (current_index == 0){
-                        console.log("Decoded Length", secret_length);
-                        var buf = new Buffer(secret_length);
+                if (counter % 8 == 0){
+
+                    if (counter == 8){
+                        secret_length = extracted_byte;
                     }
-                    
-                    if (current_index < secret_length){
-                        
-                        secret_byte = pixel_color & 3;
-                        secret_byte <<= 2;
-                        secret_byte ^= (pixel_color >> 8) & 3;
-                        secret_byte <<= 2;
-                        secret_byte ^= (pixel_color >> 16) & 3;
-                        secret_byte <<= 2;
-                        secret_byte ^= (pixel_color >> 24) & 3;
-                        
-                        buf[current_index] = secret_byte;
-                        current_index++;
+                    else{
+                        extracted_byte <<= (counter - 8);
+                        secret_length ^= extracted_byte;
                     }
+                    extracted_byte = 0;
+                    continue;    
                 }
-               
-			}
-		}
-        
-        fs.writeFile('message.png', buf, "binary", function(){
-            console.log("SUCCESS!");
-        });
-        
-	});
+            }
+            else{
+                let secret_byte = 0;
+
+                if (current_index == 0){
+                    console.log("Decoded Length", secret_length);
+                    if(secret_length < 0 || (secret_length > (width * height) - 4) ) {
+                        console.log("Error parsing the encoded file");
+                        return;
+                    }
+                    
+                    var buf = new Buffer(secret_length);
+                }
+
+                if (current_index < secret_length){
+
+                    secret_byte = pixel_color & 3;
+                    secret_byte <<= 2;
+                    secret_byte ^= (pixel_color >> 8) & 3;
+                    secret_byte <<= 2;
+                    secret_byte ^= (pixel_color >> 16) & 3;
+                    secret_byte <<= 2;
+                    secret_byte ^= (pixel_color >> 24) & 3;
+
+                    buf[current_index] = secret_byte;
+                    current_index++;
+                }
+            }
+
+        }
+    }
+
+    fs.writeFile('output_binary', buf, "binary", function(){
+        console.log("SUCCESS!");
+    });
 
 }
-
-
-
-//fs.readFile("test.jpg", function(err, data) {
-//  if (err) throw err;
-//  //encode(data);
-//  decode();
-//});
-
-//test();
-    
--- public/ClientSide.js
+++ public/ClientSide.js
@@ -1,3 +1,5 @@
+var allowed_size = 0;
+
 function uploadBallast(){
     
     let input, file;
@@ -14,10 +16,103 @@ function uploadBallast(){
         updatePrompt("ballast-size", "Please select a file before clicking 'Load'");
     }
     else {
-        file = input.files[0];
-        updatePrompt("ballast-size", "File " + file.name + " is " + file.size + " bytes in size");
+        
+        var formData = new FormData();
+        formData.append("file", input.files[0]);
+        
+        // Client-side file extension validation
+        let file_name = input.files[0].name;
+        if(  file_name.indexOf('.wav') != -1
+              || file_name.indexOf('.png') != -1
+              || file_name.indexOf('.jpg') != -1
+              || file_name.indexOf('.bmp') != -1){
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "http://localhost:8080/upload_ballast");
+                xhr.onload = function(){
+                    //console.log(xhr.response);
+                    allowed_size = parseInt(xhr.response);
+                    updatePrompt("allowed-size", xhr.response + " bytes");
+                }
+                xhr.send(formData);
+
+                file = input.files[0];
+                updatePrompt("ballast-size", "File " + file.name + " is " + file.size + " bytes in size");
+        }
+        else{
+            updatePrompt("ballast-size", "Invalid extension");
+        }
     }
 }
+
+function uploadSecret(){
+    
+    let input, file;
+    
+    if (!window.FileReader) {
+        bodyAppend("p", "The file API isn't supported on this browser yet.");
+        return;
+    }    
+    input = document.getElementById('secret-fileinput');
+    if (!input.files) {
+        updatePrompt("secret-size", "This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+        updatePrompt("secret-size", "Please select a file before clicking 'Load'");
+    }
+    else {
+        
+        var formData = new FormData();
+        formData.append("file", input.files[0]);
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:8080/upload_secret");
+        xhr.send(formData);
+
+        file = input.files[0];
+        updatePrompt("secret-size", "File " + file.name + " is " + file.size + " bytes in size");
+        
+    }
+}
+
+function uploadEncoded(){
+    
+    let input, file;
+    
+    if (!window.FileReader) {
+        bodyAppend("p", "The file API isn't supported on this browser yet.");
+        return;
+    }    
+    input = document.getElementById('decode-fileinput');
+    if (!input.files) {
+        updatePrompt("secret-size", "This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+        updatePrompt("secret-size", "Please select a file before clicking 'Load'");
+    }
+    else {
+        
+        var formData = new FormData();
+        formData.append("file", input.files[0]);
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:8080/decode");
+        xhr.send(formData);
+
+//        file = input.files[0];
+//        updatePrompt("secret-size", "File " + file.name + " is " + file.size + " bytes in size");
+//        
+    }
+}
+
+function encode(){
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/encode");
+    xhr.send();
+    
+}
+
 
 function updatePrompt(element_id, message){
     let ele = document.getElementById(element_id);
